fae59d6a0ab3319e9c9402d7e61a160d6f8c81c6
carlosdcweb <carlos@dccolorweb.com>
2016-03-27 11:01:50 -0600

    Creating the pay method. Add price to item.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -49,6 +49,19 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay
+    @order = Order.find(params[:id])
+      if @order.total_amount == params[:amount]
+        @receipt = Receipt.new(order: @order, payment_method: params[:payment_method ])
+
+        if @receipt.save
+          render json: @receipt, status: 204
+        else
+          render json: @receipt.errors, status: 422
+        end
+      end
+  end
+
   private
 
     def set_order
